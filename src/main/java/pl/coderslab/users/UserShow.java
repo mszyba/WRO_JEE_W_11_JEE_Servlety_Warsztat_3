938b249867ffe4e3c52c3f4e35b560f2c598f318
Michał Szyba <m.szyba@gmail.com>
2021-02-02 14:07:53 +0000

    add servlet UserShow

---
+++ src/main/java/pl/coderslab/users/UserShow.java
@@ -0,0 +1,28 @@
+package pl.coderslab.users;
+
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UserShow", value = "/user/show")
+public class UserShow extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        UserDao userDao = new UserDao();
+        User read = userDao.read(Integer.parseInt(id));
+        request.setAttribute("user", read);
+
+        getServletContext().getRequestDispatcher("/users/show.jsp")
+                .forward(request, response);
+    }
+}
