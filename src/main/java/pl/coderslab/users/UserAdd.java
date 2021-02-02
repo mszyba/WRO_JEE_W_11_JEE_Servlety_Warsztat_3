780a50cc66635f500e533cf1a11f20dcdeeae2d7
Michał Szyba <m.szyba@gmail.com>
2021-02-02 13:03:46 +0000

    add servlet UserAdd

---
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -0,0 +1,21 @@
+package pl.coderslab.users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UserAdd",value = "/user/add")
+public class UserAdd extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        getServletContext().getRequestDispatcher("/users/add.jsp")
+                .forward(request, response);
+    }
+}
