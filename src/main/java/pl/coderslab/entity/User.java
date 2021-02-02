9b7c1e7c5bc4f8ef40f4a5a07b6132aabc68ae77
Michał Szyba <m.szyba@gmail.com>
2021-02-02 11:35:33 +0000

    add class User

---
+++ src/main/java/pl/coderslab/entity/User.java
@@ -0,0 +1,46 @@
+package pl.coderslab.entity;
+
+public class User {
+    private Integer id;
+    private String userName;
+    private String email;
+    private String password;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
