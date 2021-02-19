038c760687f0c1a128e1465726e299e894593fa0
Michał Szyba <m.szyba@gmail.com>
2021-02-19 14:39:20 +0100

    added Homepage

---
+++ src/main/java/pl/coderslab/home/ServletHome.java
@@ -0,0 +1,16 @@
+package pl.coderslab.home;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ServletHome", value = "/home")
+public class ServletHome extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        getServletContext().getRequestDispatcher("/index.jsp")
+                .forward(request, response);
+    }
+}
