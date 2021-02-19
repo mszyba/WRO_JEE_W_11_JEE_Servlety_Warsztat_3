b089ddf119f5820c2ff346dba37cc4e886036018
Michał Szyba <m.szyba@gmail.com>
2021-02-19 14:19:24 +0100

    change .toString

--- src/main/java/pl/coderslab/entity/User.java
+++ src/main/java/pl/coderslab/entity/User.java
@@ -60,7 +60,6 @@ public class User {
                 "id=" + id +
                 ", userName='" + userName + '\'' +
                 ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
                 '}';
     }
 }
