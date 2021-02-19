e45483d1d70bb8696050ebab51d3db1f3c77e32e
Michał Szyba <m.szyba@gmail.com>
2021-02-19 20:29:06 +0100

    added check is exist email

--- src/main/java/pl/coderslab/users/UserAdd.java
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -6,23 +6,33 @@ import pl.coderslab.entity.UserDao;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet(name = "UserAdd", value = "/user/add")
 public class UserAdd extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        User user = new User();
-        user.setUserName(request.getParameter("userName"));
-        user.setEmail(request.getParameter("userEmail"));
-        user.setPassword(hashPassword(request.getParameter("userPassword")));
+        String userName = request.getParameter("userName");
+        String userEmail = request.getParameter("userEmail");
+        String userHashPassword = hashPassword(request.getParameter("userPassword"));
 
-        UserDao userDao = new UserDao();
-        userDao.create(user);
-        response.sendRedirect(request.getContextPath() + "/user/list");
+        if (!UserDao.isExistEmail(userEmail)) {
+            User user = new User();
+            user.setUserName(userName);
+            user.setEmail(userEmail);
+            user.setPassword(userHashPassword);
+
+            UserDao userDao = new UserDao();
+            userDao.create(user);
+            response.sendRedirect(request.getContextPath() + "/user/list");
+        } else {
+            Cookie cookie = new Cookie("emailExist", "true");
+            cookie.setMaxAge(5);
+            response.addCookie(cookie);
+
+            response.sendRedirect(request.getContextPath() + "/user/add");
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
