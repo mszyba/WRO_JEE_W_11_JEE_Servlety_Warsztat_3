19b980dd601fc203a3c6bccd0a8b79eb53373af9
Michał Szyba <m.szyba@gmail.com>
2021-02-20 12:27:38 +0100

    added isExistEmail

--- src/main/java/pl/coderslab/users/UserEdit.java
+++ src/main/java/pl/coderslab/users/UserEdit.java
@@ -6,6 +6,7 @@ import pl.coderslab.entity.UserDao;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,16 +15,29 @@ import java.io.IOException;
 @WebServlet(name = "UserEdit", value = "/user/edit")
 public class UserEdit extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = new User();
-        user.setId(Integer.parseInt(request.getParameter("id")));
-        user.setUserName(request.getParameter("userName"));
-        user.setEmail(request.getParameter("userEmail"));
-        user.setPassword(hashPassword(request.getParameter("userPassword")));
 
-        UserDao userDao = new UserDao();
-        userDao.update(user);
+        int userId = Integer.parseInt(request.getParameter("id"));
+        String userName = request.getParameter("userName");
+        String userEmail = request.getParameter("userEmail");
+        String userHashPassword = hashPassword(request.getParameter("userPassword"));
 
-        response.sendRedirect(request.getContextPath() + "/user/list");
+        if (!UserDao.isExistEmail(userEmail)) {
+            User user = new User();
+            user.setId(userId);
+            user.setUserName(userName);
+            user.setEmail(userEmail);
+            user.setPassword(userHashPassword);
+
+            UserDao userDao = new UserDao();
+            userDao.update(user);
+            response.sendRedirect(request.getContextPath() + "/user/list");
+        } else {
+            Cookie cookie = new Cookie("emailExist", "true");
+            cookie.setMaxAge(5);
+            response.addCookie(cookie);
+
+            response.sendRedirect(request.getContextPath() + "/user/edit?id=" + userId);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
