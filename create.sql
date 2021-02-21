94ba1fd1d79a5e7994f140a0d950ef9ab1a0fbc0
Michał Szyba <m.szyba@gmail.com>
2021-02-21 15:58:48 +0100

    added examples records to tables in db

--- create.sql
+++ create.sql
@@ -7,3 +7,13 @@ CREATE TABLE users (
                        password VARCHAR(255) DEFAULT NULL,
                        PRIMARY KEY (id)
 );
+
+INSERT INTO users (username, email, password) VALUES
+('Harry Poter',  'harry@wp.pl', 'dsd87sd65d#$'),
+('Harry Hole',  'harry@hole.com', 'dsd5trdsd65d#$'),
+('John Wick',  'john@gmail.com', 'fr3wsdsd65d#$'),
+('Jake Wood',  'jakewood@gmail.com', 'fr3d65d#$'),
+('John Deree',  'dere@hotmail.com', 'FDGTdds3d65d#$'),
+('Ann Moss',  'thebest@mail.com', '#DG7ussd578jyj'),
+('Thomas Anderson',  'neo@matrix.com', '#DG7usdscsd578jyj'),
+('Keanu Reeves',  'neo99@matrix.com', '#Dds43edssd578jyj');
